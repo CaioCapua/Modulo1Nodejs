5091ffc3a9266466554a5ce3a39842127f718e90
Caio Capua <caiocapua@hotmail.com>
2020-10-21 12:00:21 -0300

    Lib uuidv4, api semi-funcional, falta Delete e Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
-const { request } = require('express');
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' })
-})
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+    return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+    const { title, owner } = request.body;
+
+    const project = {
+        id: uuid(),
+        title,
+        owner,
+    }
+
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;
+
+    const projectsIndex = projects.findIndex(project => project.id === id);
+
+    if( projectsIndex === -1 ) {
+        response.json({ error: 'Invalid Prject ID' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects.push(project);
+
+    return response.json(project);
+ 
+});
+
+app.delete('/projects/2', (request, response) => {
+    return response.json([
+        'Projeto4',
+        'Projeto3',
+    ]);
+});
 
 app.listen(3333, () => {
     console.log('Server Started');
